84cd9df4655c1869c7360ce448638be83825c87d
Ryan Gordon <ryan.gordon@useallfive.com>
2020-04-22 21:19:02 -0700

    Upgrade all things

--- constants/Layout.js
+++ constants/Layout.js
@@ -8,25 +8,25 @@ const isSmallDevice = width <= 375 && Constants.statusBarHeight <= 25;
 const isSuperSmallDevice = width <= 320 && Constants.statusBarHeight <= 25;
 const headerHeight = Header.HEIGHT - (isSmallDevice ? Constants.statusBarHeight : Constants.statusBarHeight / 2);
 
-const _determineKeyboardHeight = (event) => {
-  let statusBarHeight = 0;
+const _determineKeyboardHeight = event => {
+    let statusBarHeight = 0;
 
-  if (Constants.statusBarHeight > 20) {
-    statusBarHeight = Constants.statusBarHeight - 20;
-  }
+    if (Constants.statusBarHeight > 20) {
+        statusBarHeight = Constants.statusBarHeight - 20;
+    }
 
-  return event.nativeEvent.layout.height + statusBarHeight;
+    return event.nativeEvent.layout.height + statusBarHeight;
 };
 
 export default {
-  window: {
-    width,
-    height
-  },
-  isSmallDevice,
-  isSuperSmallDevice,
-  headerHeight,
-  statusBarHeight: Constants.statusBarHeight,
-  device: Constants.platform[Platform.OS].model,
-  getKeyboardOffset: (event) => _determineKeyboardHeight(event)
+    window: {
+        width,
+        height
+    },
+    isSmallDevice,
+    isSuperSmallDevice,
+    headerHeight,
+    statusBarHeight: Constants.statusBarHeight,
+    device: Constants.platform[Platform.OS].model,
+    getKeyboardOffset: event => _determineKeyboardHeight(event)
 };
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,9 +1,10 @@
 import { createDrawerNavigator, createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
 import Layout from '../constants/Layout';
-
 import SideMenu from './SideMenu';
-
 import { HomeScreen } from '../screens/HomeScreen';
+
+const drawerType = Layout.window.width >= 768 ? 'permanent' : Platform.OS === 'ios' ? 'slide' : 'front';
 
 const AppDrawerNavigator = createDrawerNavigator(
     {
@@ -13,7 +14,9 @@ const AppDrawerNavigator = createDrawerNavigator(
     },
     {
         contentComponent: SideMenu,
-        drawerWidth: Layout.window.width - 100
+        drawerWidth: Layout.window.width * 0.75,
+        overlayColor: 'rgba(0, 0, 0, 0.7)',
+        drawerType
     }
 );
 
